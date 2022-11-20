5982ae2f2aa5004a8b608c9ac020b3a220485a36
Enes BULUT <74255515+Enesbulu@users.noreply.github.com>
2022-11-20 14:14:18 +0300

    Ders14

---
+++ .NET eğitimi/C#Dersleri/Ders14_Ornekler/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Threading;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Ders14_Ornekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ornek1
+            /*
+            //Kullanıcıdan 2 adet sayı talep edelim. Büyük sayidan küçük sayıya kadar 3'er 3'er ekrana yazıralım
+            Console.Write("Bir sayı giriniz: ");
+            short sayi1 = short.Parse(Console.ReadLine());
+            Console.Write("Bir sayı giriniz: ");
+            short sayi2 = short.Parse(Console.ReadLine());
+            if (sayi1 > sayi2)
+            {
+                short temp = sayi1;
+                sayi1 = sayi2;
+                sayi2 = temp;
+            }
+            Console.WriteLine($"Büyük Sayı: {sayi2}, Küçük sayi: {sayi1}");
+            for (int i = sayi2; i >= sayi1; i -= 3)
+            {
+                Console.Write(i + " ");
+            }
+            */
+            #endregion
+
+            #region Ornek2
+            //kullanıcıcan 10 adet sayı talep edelim. Talep edilen sayıların toplamını ekranan yazdıralım.
+            /* short toplam = 0;
+             for (int i = 1; i <= 10; i++)
+             {
+                 Console.Write($"\nLütfen {i}. sayı giriniz: ");
+                 toplam += short.Parse(Console.ReadLine());
+             }
+             Console.WriteLine("Toplam: "+toplam);
+
+             */
+
+            #endregion
+
+            #region Ornek3
+
+            //kullanıcıya kaç adet sayı girmek isteğini sorun. Girmek istediği sasyı kadar kullanıcıdan deger talep edin. girilen degerlerden en küçüğünü ve en büyüğünü ekrana yazdırın.
+            /* Console.Write("Kaç adet sayı girmek istiyorsunız? : ");
+             short donguayisi = short.Parse(Console.ReadLine());
+             short enBuyuk = 0, enKucuk = 0, temp = 0;
+             for (int i = 0; i < donguayisi; i++)
+             {
+                 if (i == 0)
+                 {
+                     Console.Write((i + 1) + ". Sayıyı giriniz: ");
+                     temp = short.Parse(Console.ReadLine());
+                     enKucuk = temp;
+                     enBuyuk = enKucuk;
+                 }
+                 else
+                 {
+                     Console.Write((i + 1) + ". Sayıyı giriniz: ");
+                     temp = short.Parse(Console.ReadLine());
+                     if (enBuyuk < temp)
+                     {
+                         enBuyuk = temp;
+                     }
+                     if (enKucuk > temp)
+                     {
+                         enKucuk = temp;
+                     }
+                 }
+
+
+             }
+             Console.WriteLine($"En küçük sayı: {enKucuk}");
+             Console.WriteLine($"En büyük sayı: {enBuyuk}");
+             */
+            #endregion
+
+            #region Ornek4
+
+            //Kullanıcıdan 2 adet sayı talep edelim. 1. sayının 2. sayıya üssünü hesaplayalım.
+            /* Console.Write("Taban degerini giriniz: ");
+             short taban = short.Parse(Console.ReadLine());
+             Console.Write("Üs sayısını giriniz: ");
+             short usSayısı= short.Parse(Console.ReadLine());
+             int sonuc = 1;
+             for (int i = 0; i < usSayısı; i++)
+             {
+                 sonuc *= taban;
+             }
+             Console.WriteLine($"{taban} sayısnın {usSayısı}. kuvveti : {sonuc}");*/
+            #endregion
+
+            #region Ornek5
+           /* //Belirtilen en ve boy degerine göre dikdörtgen degerini çizdiriniz.
+            Console.Write("En bilgisi giriniz: ");
+            byte enBilgisi = byte.Parse(Console.ReadLine());
+            Console.Write("Boy Bilgisi Giriniz: ");
+            byte boyBilgisi = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < boyBilgisi; i++)
+            {
+                for (int j = 0; j < enBilgisi; j++)
+                {
+                    if (i == 0 || i ==boyBilgisi-1)
+                    {
+                        for (int l = 0; l < enBilgisi-1; l++)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();                        
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < (enBilgisi-2); k++)
+                        {
+                            Console.Write("* ");
+                        }
+                    }
+                    
+                }
+            }
+
+          
+            */
+            #endregion
+
+            #region Ornek6
+            //Belirtilen en ve boy bilgisine göre satranç tahtası çizelim.
+           /* Console.Write("Yatay kutucuk sayısı giriniz: ");
+            byte yatay = byte.Parse(Console.ReadLine());
+            Console.Write("Düşey kutucuk sayısı giriniz: ");
+            byte dusey = byte.Parse(Console.ReadLine());
+            for (int i = 0; i < dusey; i++)
+            {
+                for (int j = 0; j < yatay; j++)
+                {
+                    if (i %2 ==0 && j %2 ==0)
+                    {
+                        Console.BackgroundColor= ConsoleColor.Black;
+                        Console.Write("  ");
+                    }
+                    else if (i %2 ==0 &&   j % 2 ==1 )
+                    {
+                        Console.BackgroundColor= ConsoleColor.White;
+                        Console.Write("  ");
+                    }
+                    else if (i %2 ==1 && j%2==0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }*/
+
+            #endregion
+
+
+
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ .NET eğitimi/C#Dersleri/Ders14_Random/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Ders14_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Random
+            //Random(Rastgele)
+            //-------------------------------------
+            //Rastgele sayı üretme işlemi Random sınıfı kullanarak içerisinde bulunan metotlar yardımı ile oluşturulurlar.
+            //Syntax
+            //Random <degiskenAdi> = new Random();  //Bu yazım kuralı ile Random yazım kuralı ile Random sayı üretmek için gerekli olan nesneyi program içerisinde tanımlamış oluruz.
+            /*  Random rnd = new Random();
+
+              //Rastgele tam sayı oluşturma.
+              int rsayi = rnd.Next(); // Next() metodu 0 ile 2.147.483.647 sayı aralığında bir tam sayı üretmek için kullanılır. Geriye int türünde deger döndürür.
+
+              //Belirtilen sayıya kadar random deger oluşturma.
+              int rsayi2 = rnd.Next(50);  //0 ile 50 sayısına kadar (50 hariç) olan degerler arasında rastgele bir tam sayı üretir. Geriye int deger döndürür.
+
+              //Belirtilen aralıkta tam sayı oluşturma.
+              int rsayi3 = rnd.Next(50,100);  //50 ile 100 arasında (100 hariç) rastgele bir tam sayı üretir.
+
+              //Ondalıklı sayi oluşturma
+              double rsayı4 = rnd.NextDouble();   //0.0 ile 1.0 (1.0 hariç) arasında rastgele olarak ondalık sayı üretir. Geriye double türünde bir deger döndürür.
+
+              Console.Clear();    //Consol ekranında yazılan metinleri siler.
+
+              */
+            #endregion
+
+            #region Ornek1
+
+            //5 adet rastgele sayı üretip ekrana yazdıralım.
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                //1 ila 10 arasında rastgele sayı elde etme
+                Console.WriteLine(random.Next(1,11));
+
+            }
+
+            Console.Clear();
+            #endregion
+
+            #region Ornek2
+
+            //A karakterinden Z karakteri arasında rastgele bir harf oluşturalım.
+
+            /*int rsayi5 = random.Next(65,91);
+            char karakter = Convert.ToChar(rsayi5);
+            Console.WriteLine("Oluşturulan rastgele karakter: "+ karakter);
+            int baslangic = Convert.ToInt32('A');
+            int bitis = Convert.ToInt32('Z');
+            int sayi6 = random.Next(baslangic,bitis+1);
+            Console.WriteLine("Oluşturulan rastgele karakter: " + sayi6);
+            Console.WriteLine("Oluşturulan rastgele karakter: " +(Convert.ToString(random.Next(baslangic, bitis + 1))));
+            Console.WriteLine("Oluşturulan rastgele karakter: " + (Convert.ToString(sayi6)));
+            */
+            #endregion
+
+
+
+
+            Thread.Sleep(3000);
+        }
+    }
+}
