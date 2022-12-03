e18e80f440b68781062d78c5283005cdb8deb94d
Enes BULUT <74255515+Enesbulu@users.noreply.github.com>
2022-12-03 10:12:03 +0300

    Rehber Uygulaması

--- .NET eğitimi/C#Dersleri/Ders15_List/Program.cs
+++ .NET eğitimi/C#Dersleri/Ders15_List/Program.cs
@@ -10,125 +10,125 @@ namespace Ders15_List
         static void Main(string[] args)
         {
             #region List
-            /* //List<T> koleksiyonu kendi içerisinde belirtilen tipe göre eleman barındıran bir koleksiyon yapısında sahiptir. Dizilerde olduğu gibi ilk olulturma işlemi esnasında içerisinde kaç adet eleman barınıracağının bilgisinin verilmesine gerek yoktur. Koleksiyon oluşturulduktan sonra içerisine eklenen ve çıkarılan elemanlara göre otomtik olarak boyutunu kendisi ayarlamaktadır. Bu sayede dizilerde olduğu gibi boyut ayarlama işlemine gerek yoktur.
-             //List<T> yapısındaki T karakteri programlama dili içerisindeki bir tipi temsil eder. Bu tipler veritipi, sınıfı veya nesne olabilir. T alanı içerisinde tanımlanan türe göre veri barındırabilir. Herhangi bir boyut kısıtlaması yoktur.
+            //List<T> koleksiyonu kendi içerisinde belirtilen tipe göre eleman barındıran bir koleksiyon yapısında sahiptir. Dizilerde olduğu gibi ilk olulturma işlemi esnasında içerisinde kaç adet eleman barınıracağının bilgisinin verilmesine gerek yoktur. Koleksiyon oluşturulduktan sonra içerisine eklenen ve çıkarılan elemanlara göre otomtik olarak boyutunu kendisi ayarlamaktadır. Bu sayede dizilerde olduğu gibi boyut ayarlama işlemine gerek yoktur.
+            //List<T> yapısındaki T karakteri programlama dili içerisindeki bir tipi temsil eder. Bu tipler veritipi, sınıfı veya nesne olabilir. T alanı içerisinde tanımlanan türe göre veri barındırabilir. Herhangi bir boyut kısıtlaması yoktur.
 
-             //Syntax
-             //----------
-             //List<T> <degiskenAdi> = new List<T>();
-             #endregion
+            //Syntax
+            //----------
+            //List<T> <degiskenAdi> = new List<T>();
+            #endregion
 
-             #region Kullanımı
+            #region Kullanımı
 
-             //İçerisinde string türünden degerler barındıran bir list koleksiyonu oluşturalım.
-             List<string> sehirler = new List<string>();
+            //İçerisinde string türünden degerler barındıran bir list koleksiyonu oluşturalım.
+            List<string> sehirler = new List<string>();
 
-             //koleksiyon içerisinde bir eleman ekleme işlemi yapmak için Add() metodu kullanılır. Bu metod içerisine, koleksiyon içerisinde barındıran tipte deger alır.
+            //koleksiyon içerisinde bir eleman ekleme işlemi yapmak için Add() metodu kullanılır. Bu metod içerisine, koleksiyon içerisinde barındıran tipte deger alır.
 
-             //Koleksiyon içerisinde bir eleman eklemek için 
-             //<koleksiyonAdi>.Add(<T>);
+            //Koleksiyon içerisinde bir eleman eklemek için 
+            //<koleksiyonAdi>.Add(<T>);
 
-             sehirler.Add("Ankar");
-             sehirler.Add("İstanbul");
-             sehirler.Add("İzmir");
-             sehirler.Add("Antalya");
-             sehirler.Add("Manisa");
+            sehirler.Add("Ankar");
+            sehirler.Add("İstanbul");
+            sehirler.Add("İzmir");
+            sehirler.Add("Antalya");
+            sehirler.Add("Manisa");
 
-             //List<T> koleksiyonu içerisinde barındırılan eleman sayısını öğrenmek için count property sini kullanabilirsiniz. Eleman Sayısını tam türünden bilgisini verir.
+            //List<T> koleksiyonu içerisinde barındırılan eleman sayısını öğrenmek için count property sini kullanabilirsiniz. Eleman Sayısını tam türünden bilgisini verir.
 
-             //<koleksiyon>.Count
-             Console.WriteLine("Sehirler koleksiyonun eleman sayısı : " + sehirler.Count);
-             Console.WriteLine();
+            //<koleksiyon>.Count
+            Console.WriteLine("Sehirler koleksiyonun eleman sayısı : " + sehirler.Count);
+            Console.WriteLine();
 
-             //Sehirler koleksiyonunun elemanlarına index numaraları aracılığıyla ulaşım sağlayabilirsiniz.
-             for (int i = 0; i < sehirler.Count; i++)
-             {
-                 Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
+            //Sehirler koleksiyonunun elemanlarına index numaraları aracılığıyla ulaşım sağlayabilirsiniz.
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
 
-             }
-             #endregion
+            }
+            #endregion
 
-             #region Propertileri
-
-
-             //List<T> koleksiyonu içerisindeki bir elemanı silmek için 2 farkı yöntem buunmaktadır.
-             //1.Yontem=>RemoveAt(<index>) metodu ile index numarasına bağlı olarak belirtilen indexteki degeri siler.
-             //2.Yöntem => Remove(<deger>) metodu ile belirtilen degere göre silme işlemi yapar. koleksiyon içerisindeki ilk veriyi siler.
-
-             sehirler.Add("İzmir");
-             Console.WriteLine();
-             for (int i = 0; i < sehirler.Count; i++)
-             {
-                 Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
-
-             }
-             Console.WriteLine();
-             sehirler.RemoveAt(4);
-
-             for (int i = 0; i < sehirler.Count; i++)
-             {
-                 Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
-
-             }
-             sehirler.Remove("İzmir");
+            #region Propertileri
 
 
-             //List<T> koleksiyonu içerisindeki bir eleman yeni deger atamak
-             sehirler[2] = "Sivas";
-             for (int i = 0; i < sehirler.Count; i++)
-             {
-                 Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
+            //List<T> koleksiyonu içerisindeki bir elemanı silmek için 2 farkı yöntem buunmaktadır.
+            //1.Yontem=>RemoveAt(<index>) metodu ile index numarasına bağlı olarak belirtilen indexteki degeri siler.
+            //2.Yöntem => Remove(<deger>) metodu ile belirtilen degere göre silme işlemi yapar. koleksiyon içerisindeki ilk veriyi siler.
 
-             }
-             Console.WriteLine();
+            sehirler.Add("İzmir");
+            Console.WriteLine();
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
 
-             //List<T> koleksiyonu içerisindeki elemanları sıralama işlemi
-             sehirler.Sort();
+            }
+            Console.WriteLine();
+            sehirler.RemoveAt(4);
 
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
 
-             //List<T> koleksiyonu içerisindeki elemanları tersten sıralama işlemi
-             sehirler.Reverse();
-
-             //Koleksiyon içerisindeki elemanları siler.
-             sehirler.Clear();
-
-             //Koleksiyon içerisinde belirtilen deger arar. Deger var ise true deger yok ise false degeri döner.
-             sehirler.Contains("İstanbul");
-
-
-             //Koleksiyon içerisinde belirtlen indexe belirtilen degeri ekler.
-             sehirler.Insert(1, "Manisa");
-
-             //Koleksiyon içerisineki son elemanı veriri
-             sehirler.Last();
+            }
+            sehirler.Remove("İzmir");
 
 
-             //Koleksiyon içerisindeki sayısal degerlerde en büyük degeri geri döndürür.
-             Console.WriteLine(sehirler.Max());
+            //List<T> koleksiyonu içerisindeki bir eleman yeni deger atamak
+            sehirler[2] = "Sivas";
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                Console.WriteLine(i + ". indexteki eleman: " + sehirler[i]);
+
+            }
+            Console.WriteLine();
+
+            //List<T> koleksiyonu içerisindeki elemanları sıralama işlemi
+            sehirler.Sort();
 
 
-             //Koleksiyon içerisindeki sayısal degerlerde en küçük degeri geri döndürür.
-             Console.WriteLine(sehirler.Min());
+            //List<T> koleksiyonu içerisindeki elemanları tersten sıralama işlemi
+            sehirler.Reverse();
 
-             //Kolaksiyonu array'e çevirir
-             sehirler.ToArray();
+            //Koleksiyon içerisindeki elemanları siler.
+            sehirler.Clear();
 
-             //Array'i List<T> yapısına çevirme
-             double[] rakamlar = { 12.5, 7.45, 8, 85 };
-             List<double> liste = rakamlar.ToList();
+            //Koleksiyon içerisinde belirtilen deger arar. Deger var ise true deger yok ise false degeri döner.
+            sehirler.Contains("İstanbul");
 
 
-             //Koleksiyon içerisinde aranan deger deger yoksa -1 degerini, varsa index numarası döner. Aynı deger birden fazla varsa ilk degeri döndürür.
-             sehirler.IndexOf("Ankara");
+            //Koleksiyon içerisinde belirtlen indexe belirtilen degeri ekler.
+            sehirler.Insert(1, "Manisa");
 
-             //IndexOf() Metodunun tersine arama işlemi yapar.
-             sehirler.LastIndexOf("Mersin");
-             //Koleksiyon içerisinde tek komut satırlık bir işlem yapılacak ise foreach() metodu kullanılabilir.
-             //Foreach metodu içeriisnde bir lambda tanımlaması yapılmaktadır. => ifadesinden sonra komut satırınızı uygulayabilirsiniz.
-             //<koleksiyonAdi>ForEACh(<takmaAd> => <komut>);
-             sehirler.ForEach(sehir => Console.WriteLine(sehir));
+            //Koleksiyon içerisineki son elemanı veriri
+            sehirler.Last();
 
-             */
+
+            //Koleksiyon içerisindeki sayısal degerlerde en büyük degeri geri döndürür.
+            Console.WriteLine(sehirler.Max());
+
+
+            //Koleksiyon içerisindeki sayısal degerlerde en küçük degeri geri döndürür.
+            Console.WriteLine(sehirler.Min());
+
+            //Kolaksiyonu array'e çevirir
+            sehirler.ToArray();
+
+            //Array'i List<T> yapısına çevirme
+            double[] rakamlar = { 12.5, 7.45, 8, 85 };
+            List<double> liste = rakamlar.ToList();
+
+
+            //Koleksiyon içerisinde aranan deger deger yoksa -1 degerini, varsa index numarası döner. Aynı deger birden fazla varsa ilk degeri döndürür.
+            sehirler.IndexOf("Ankara");
+
+            //IndexOf() Metodunun tersine arama işlemi yapar.
+            sehirler.LastIndexOf("Mersin");
+            //Koleksiyon içerisinde tek komut satırlık bir işlem yapılacak ise foreach() metodu kullanılabilir.
+            //Foreach metodu içeriisnde bir lambda tanımlaması yapılmaktadır. => ifadesinden sonra komut satırınızı uygulayabilirsiniz.
+            //<koleksiyonAdi>ForEACh(<takmaAd> => <komut>);
+            sehirler.ForEach(sehir => Console.WriteLine(sehir));
+
+
 
             #endregion
 
@@ -160,7 +160,7 @@ namespace Ders15_List
             //}
 
             Console.WriteLine("Sayıların toplamı : " + toplam);
-            Console.WriteLine("Sayılaın ortalaması: " + (double)toplam/sayilar.Count);
+            Console.WriteLine("Sayılaın ortalaması: " + (double)toplam / sayilar.Count);
 
 
             #endregion
---
+++ .NET eğitimi/C#Dersleri/Odev7_RehberUygulamasi/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace Odev7_RehberUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Rehber Uygulaması
+            ---------------------------------
+            E- Kişi Ekle (Sadece ad soyad bilgisi alınarak string olarak kayıt edilecek.)
+            L- Kişileri Listele (Koleksiyon içerisindeki isimler listelenecek.)
+            S- Kişi Sil (Koleksiyon içerisindeki kişiler index numarasına göre silinecek.)
+            A- Kişi Ara (Aramak istediğiniz metni girdikten sonra kişi adında bu değerleri barındıran kişiler varsa onlar listelenecek.)
+            T- Silinen Kişiler (Silinen kişiler görüntülenecek.)
+            X- Programdan Çık (Programda çıkılacak)
+            */
+            Console.WriteLine("Rehber Uygulamasına Hoşgeldiniz.");
+            List<string> adSoyad = new List<string>();
+            List<string> silinenKisiler = new List<string>();
+            bool durum = true;
+
+            do
+            {
+                Console.WriteLine("\n\nE- Kişi Ekle\nL- Kişileri Listele\nS- Kişi Sil\nA- Kişi Ara\nT- Silinen Kişiler\nX- Programdan Çık");
+                Console.Write("Yapmak İstediğiniz İşlemi Seçiniz: ");
+                char islem=' ';
+                try
+                {
+                    islem = char.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Hata!! >> " + e);
+                }
+                
+                switch (islem)
+                {
+                    #region Kisi Ekle
+                    case 'e':
+                    case 'E':
+                        Console.Clear();
+                        Console.WriteLine("--Bir Üst menüye dönmek için (x) 'e basınız.--");
+                        for (; ; )
+                        {
+                            Console.Write("İsin - Soyisim Giriniz : ");
+                            string temp = Console.ReadLine();
+                            if ("x" == temp)
+                            {
+                                break;
+                            }
+                            adSoyad.Add(temp);
+                        }
+                        adSoyad.Sort();
+                        break;
+                    #endregion
+
+                    #region Kisi Listele
+                    case 'l':
+                    case 'L':
+                        Console.Clear();
+                        foreach (var kisi in adSoyad)
+                        {
+                            Console.WriteLine(kisi);
+                        }
+                        break;
+                    #endregion
+
+                    #region Kisi Sil
+                    case 's':
+                    case 'S':
+                        Console.Clear();
+                        for (int i = 0; i < adSoyad.Count; i++)
+                        {
+                            Console.WriteLine((i + 1) + " - " + adSoyad[i]);
+                        }
+                        Console.Write("Silemek istediğiniz kişinin sıra numarasını giriniz: ");
+                        short sira = short.Parse(Console.ReadLine());
+                        silinenKisiler.Add(adSoyad[sira-1]);
+                        adSoyad.RemoveAt(sira - 1);
+                        Console.WriteLine("\nKayıt başarıyla silindi.");
+                        break;
+
+                    #endregion
+
+                    #region Kisi Arama
+                    case 'a':
+                    case 'A':
+                        Console.Clear();
+                        Console.Write("Aramak istediğiniz Kişiyi yazınız: ");
+                        string isim = Console.ReadLine();
+                        int sirasi = (adSoyad.IndexOf(isim));
+                        if (sirasi >= 0)
+                        {
+                            Console.WriteLine($"\nAranan Kişi bulundu.\n{sirasi + 1}-{adSoyad[sirasi]}");
+                        }
+                        else if (sirasi < 0)
+                        {
+                            Console.Write("\nAradığınız isim bulunamadı.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nBir hatayla karşılaşılı!");
+                        }
+
+                        break;
+                    #endregion
+
+                    #region Silinen Kisiler
+                    case 't':
+                    case 'T':
+                        Console.Clear();
+                        foreach (var silinenler in silinenKisiler)
+                        {
+                            Console.WriteLine("Silinen Kişiler:\n-----------\n" + silinenler);
+                        }
+                        
+                        break;
+                    #endregion
+
+                    #region Cıkıs
+                    case 'x':
+                    case 'X':
+                        Console.Clear();
+                        Console.WriteLine("Çıkış Yapılıyor..");
+                        durum = false;
+                        break;
+
+                        #endregion
+
+                    default: Console.WriteLine("Hatalı Bir giriş yapıldı!!");
+                        break;
+                    
+                }
+                
+
+            } while (durum);
+
+
+
+            Thread.Sleep(2000);
+        }
+    }
+}
