412f1d7e6d1c1e7a36ce8fb94aef2936f5224f56
Enes BULUT <74255515+Enesbulu@users.noreply.github.com>
2022-12-04 14:09:27 +0300

    Ders18

---
+++ .NET eğitimi/C#Dersleri/Ders18_DateTime/Program.cs
@@ -0,0 +1,171 @@
+﻿#region DateTime
+//  DateTime
+//-------------------
+//Zaman ile ilgili yapabileceğiniz tüm işlemleri ve tüm verileri elde edebildiğiniz nesnedir.
+
+#region .Now
+using System.Globalization;
+
+DateTime tarih = DateTime.Now; //Şimdiki zamanı alır. Geriye DAteTime tipine deger döndürür.
+Console.WriteLine("Şimdiki Zaman: " + tarih);
+
+#endregion
+
+#region Tarih Bilgisi alma
+Console.WriteLine("Kısa tarih bilgisi: "+tarih.ToShortDateString());
+Console.WriteLine("Uzun zaman bilgisi "+ tarih.ToLongDateString());
+#endregion
+
+#region Zaman Biilgisi Alma
+Console.WriteLine("Kısa zaman bilgisi: "+tarih.ToShortTimeString());
+Console.WriteLine("Uzun zaman bilgisi: " + tarih.ToLongTimeString());
+
+
+#endregion
+
+#region Tarih Bilgisi İçerisinden Ulaşılabilecek bilgiler
+#region Gün
+Console.WriteLine("Gün: "+ tarih.Day);
+
+#endregion
+#region Ay
+Console.WriteLine("Ay: " + tarih.Month);
+#endregion
+#region Yıl
+Console.WriteLine("Yıl: "+ tarih.Year);
+#endregion
+#region Saat
+Console.WriteLine("Saat: " + tarih.Hour);
+#endregion
+#region Dakika
+Console.WriteLine("Dakika: "+tarih.Minute);
+#endregion
+#region Saniye
+Console.WriteLine("Saniye: "+tarih.Second);
+#endregion
+#region Milisaniye
+Console.WriteLine("Milisaniye: "+ tarih.Millisecond);
+#endregion
+#region Haftanın Günü
+Console.WriteLine("Haftanın Günü: "+ tarih.DayOfWeek);
+#endregion
+#region Yılın kaçıncı günü
+Console.WriteLine("Yılın kaçıncı günü: "+ tarih.DayOfYear);
+#endregion
+
+#region Artık Yıl
+//DateTime.IsLeapYear metodu artık yıl bilgisini verir. Şubat ayının 29 çektiği yılda true degerini verir.
+bool artikYilMi = DateTime.IsLeapYear(tarih.Year);
+Console.WriteLine("Artık yıl durumu: " + (artikYilMi ? "Evet" : "Hayır"));
+
+
+#endregion
+
+#endregion
+
+#endregion
+
+Console.Clear();
+
+#region Tarih Formatlama
+//Tarih Formatlama karakterleri
+//Formatlama biçimi ile yazdırma işlemi ToString() extension metodu ile uygulanır.
+
+/*yyyy => uzun yıl bilgisi
+*yy => kısa yıl bilgisi
+*MMMM => uzun ay bilgisini verir
+*MMM => Kısa ay bilgisi verir
+*MM => Sayısal olarak ayı verir //01
+*M => Sayısal olarak ayı verir //1
+*dddd => Uzun gün adını verir
+*ddd => kısa gün adını verir
+*dd => Sayısal olarak günü verir //01
+*d => Sayısal olarak günü verir //1
+*hh => 12 saatlik dilimde saat bilgisini verir
+*HH => 24 saatlik dilimde saat bilgisini verir
+*mm => dakika bilgisini verir
+*ss => saniye bilgisini verir
+*zzz => milisaniye bilgisini verir.
+*/
+
+Console.WriteLine("Custom tarih formatı: " +tarih.ToString("dd.MM.yyyy HH:mm"));
+Console.WriteLine("Custom tarih formatı: " +tarih.ToString("dd MM yyyy dddd HH:mm"));
+Console.WriteLine();
+Console.WriteLine("5 gün eklenmiş tarih: "+ tarih.AddDays(5));
+Console.WriteLine("5 gün çıkarılmış tarih: "+ tarih.AddDays(-5));
+Console.WriteLine("14 ay eklenmiş tarih: "+tarih.AddMonths(14));
+
+Console.WriteLine("2 Yıl çıkartılmış tarih: " + tarih.AddYears(-2));
+Console.WriteLine("10 saat eklenmiş tarih: "+tarih.AddHours(10));
+Console.WriteLine("1250 dk çıkarılmış tarih: " + tarih.AddMinutes(-1250));
+Console.WriteLine("10547854 saniye çıkarılmış tarih: " + tarih.AddSeconds(10547854));
+Console.WriteLine("45684345632 milisaniye çıkarılmış tarih: "+tarih.AddMilliseconds(45684345632));
+
+
+
+
+
+
+
+#endregion
+
+Console.Clear();
+
+#region Tarih Girdisi alma
+//DateTime.Parse() metodu ile string ifadeleri içerisindeki degerleri tarih formatına çevirebilir, üzerinden  tarih formatında işlemler yapabilirsiniz. Aynı işlemi Convert.ToDateTime() ile de uygulayabilirsiniz.
+
+string deger = "12.05.2022";
+DateTime tarih2 = DateTime.Parse(deger);
+DateTime tarih3 = Convert.ToDateTime(deger);
+
+Console.WriteLine("Girilen tarih bilgisi: " + tarih2);
+Console.WriteLine("Girilen tarih bilgisi: "+tarih3);
+
+Console.WriteLine();
+
+#endregion
+
+Console.Clear();
+
+#region Manuel olarak tarih oluşturma
+//Manuel olarak tarih oluşturma
+DateTime tarih4 = new DateTime(2021, 3, 15, 22, 15, 45);
+Console.WriteLine("Manuel loluşturulan tarih: " + tarih4);
+
+#endregion
+
+Console.Clear();
+
+#region TimeSpan()
+//İki tarih arasındaki farkı gün,saat,dakika,saniye, milisaniye cinsinden hesaplamanızı sağlayan bir nesnedir.
+//iki tarih bilgisi standart çıkarma işlemi yaptığınızda bu formatta bir nesne döner.
+
+TimeSpan fark = tarih - tarih2;
+//Yukarıdaki çıkarma işlemi DateTime sınfı içerisinde bulunan extension metod ile de uygulanabilmektedir.
+TimeSpan fark2 = tarih.Subtract(tarih2);
+
+Console.WriteLine(tarih + " - " + tarih2 + " arasındaki zaman farkları");
+Console.WriteLine("----------------");
+
+#region Gün farkı
+Console.WriteLine("Gün farkı : "+fark.TotalDays);
+#endregion
+#region Saat Farkı
+Console.WriteLine("Saat Farkı: "+ fark.TotalHours);
+#endregion
+#region Dakika Farkı
+Console.WriteLine("Dakika Farkı: "+ fark.TotalMinutes);
+#endregion
+#region Saniye Farkı
+Console.WriteLine("Saniye Farkı:"+fark.TotalSeconds);
+#endregion
+#region Milisaniye farkı
+Console.WriteLine("Milisaniye Farkı:"+fark.TotalMilliseconds);
+#endregion
+
+
+#endregion
+
+
+
+Thread.Sleep(2500);
---
+++ .NET eğitimi/C#Dersleri/Ders18_MathsFunctions/Program.cs
@@ -0,0 +1,111 @@
+﻿
+#region Math Sınıfı
+//   Math Sınıfı
+//-----------------------
+//Microsoft'un hazır olarak sunmuş olduğu bazı matematiksel işlemleri ekstra efor safetmeden hesaplamanızı sağlayan bir sınıftır. Sınıf içerisinde bulunan property ve fonksiyonlar ile bu işlemleri  gerçekleştirebilirsiniz.
+//Math. işleminden sonra property veya fonksiyonlara ulaşım sağlayabilirsiniz.
+
+#region PI Sayisi
+//PI sayısını veren property'dir. Bu property sadece okunabilir özelliğe sahiptir. Yeni ir deger ataması yapılamaz.
+Console.WriteLine("PI Sayisi: "+ Math.PI);
+
+#endregion
+
+#region E Sayisi(ELen)
+//E sayısını veren property'dir. Bu property sadece okunabilir özelliğe sahiptir. Yeni bir deger ataması yapılamaz.
+Console.WriteLine("E sayisi: "+Math.E);
+
+#endregion
+
+#region Abs(x)
+//Parametre olarak verilen degerin mutlak degrini hesaplayıp geri döndürür.
+Console.WriteLine("-5 sayısının mutlak degeri: " + Math.Abs(-5));
+
+#endregion
+
+#region Floor(x)
+//Parametre olarak verilen ondalık değeri alt tam sayıya yuvarlar.
+Console.WriteLine("12.98 sayısının alt tam sayısıya yuvarlanmış hali: " + Math.Floor(12.98));
+
+#endregion
+
+#region Ceiling(x)
+//Parametre olarak verilen ondalık değeri üst tam sayıya yuvarlar.
+Console.WriteLine("12.5 sayısının üst sayıya yuvarlanmış degeri: " + Math.Round(12.5));
+
+#endregion
+
+#region Round(x)
+//parametre olarak verilen ondalık degeri standart olarak yuvarlama işlemini yapar.
+Console.WriteLine("15.5 sayısının yuvarlanmış hali : "+ Math.Round(12.5));
+#endregion
+
+#region Truncate(x)
+//Parametre olarak verilen ondalık degerin ondalık kısmını atar. Geriye tam sayı değerini veriri.
+Console.WriteLine("12.65 sayısının  tam sayı hali: "+Math.Truncate(12.65));
+#endregion
+
+#region Max(x,y)
+//Parametre olarak verilen iki egern en büyüğü olanını geriye döndürür.
+Console.WriteLine("14 ile 24.5 sayılarının büyüğü: " + Math.Max(14, 24.5));
+#endregion
+
+#region Min(x,y)
+//parametre olarak verilen iki sayıdan küçük olanını verir.
+Console.WriteLine("14 ile 24.5 sayılarının küçüğü: " + Math.Min(14, 24.5));
+
+#endregion
+
+#region Pow(x,y)
+//Parametere olarak iki adet deger alır. İlk deger hangisi sayının kuvveti alınacağı, ikinci parametre kaçıncı kuvvetinin alınacağını belirtir.
+Console.WriteLine("5 syısının 3. kuvveti: ",Math.Pow(5,3));
+#endregion
+
+#region Squart(x)
+//Parametre olarak verilen degerin karakökünü hesaplar
+Console.WriteLine("9 sayısının karekökü: ", Math.Sqrt(9));
+#endregion
+
+#region Sing(x)
+//Parametre olarak verilen sayının negatif, pozitif ya da sıfır(0) 'a eğişt olup olmadığının bilgisini verir.
+//Negatif ise -1, pozitif ise 1, sıfıra eşit ise 0 degerini döndürür.
+Console.WriteLine("-25 sayısının durumu: "+Math.Sign(-25));
+int durum = Math.Sign(-25);
+if (durum==0)
+{
+    Console.WriteLine("Sıfıra eşit");
+}
+else if (durum == -1)
+{
+    Console.WriteLine("Sayı Negatif.");
+}
+else
+{
+    Console.WriteLine("Sayı pozitif.");
+}
+#endregion
+
+#endregion
+
+Console.Clear();
+
+#region Ornek
+//Kullanıcıdan 3 adet sayı talep edelim.
+//En büyük olan sayıyı ve en büyük küçük sayıyı ekrana yazdıralım
+Console.WriteLine("3 Adet Sayi giriniz");
+Console.Write("1. Sayiyı giriniz: ");
+int sayi1=int.Parse(Console.ReadLine());
+Console.Write("2. sayiyi giriniz: ");
+int sayi2 =int.Parse(Console.ReadLine()); 
+Console.Write("3. sayiyi giriniz: ");
+int sayi3 =int.Parse(Console.ReadLine());
+
+Console.Write("Girilen en küçük sayi: " + Math.Min(Math.Min(sayi1, sayi2), sayi3));
+Console.Write("\nGirilen en büyük sayi: " + Math.Max(sayi1, Math.Max(sayi2, sayi3)));
+
+
+
+
+#endregion
+
+Thread.Sleep(5000);
